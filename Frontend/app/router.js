7c229e2323811bef05d8e251acb4211cabc6eb9a
刘文同 <18911761491@163.com>
2018-11-29 10:56:26 +0800

    发布 1.14.17 版本

--- Frontend/app/router.js
+++ Frontend/app/router.js
@@ -24,12 +24,38 @@ class Routes extends React.Component {
     store: PropTypes.object.isRequired,
   }
 
+  componentWillMount() {
+    http.instance.interceptors.response.use((response) => (response),
+    (error) => {
+      const {
+        code,
+        config: {
+          method,
+          url,
+        },
+        response: {
+          status: statusCode,
+        } = {},
+        message,
+      } = error
+      console.log(error)
+      if (statusCode === 401) {
+        if (location.pathname !== '/login') {
+          /* 当用户不在登录页面时，不再显示错误信息，直接跳转到登录页面 */
+          history.replace('/login')
+        }
+        return Promise.reject(error)
+      }
+
+    })
+  }
+
   render() {
     return (
       <Fragment>
         <Route path='/' component={Home} exact strict />
-        <Route path='/signup' component={Signup} exact strict />
         <Route path='/signup_profile' component={SignupProfile} exact strict />
+        <Route path='/signup' component={Signup} exact strict />
         <Route path='/login' component={Login} exact strict />
       </Fragment>
     )
